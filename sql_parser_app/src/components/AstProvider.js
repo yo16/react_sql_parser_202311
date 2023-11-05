9a2e2077268e31e4e5717fc5d9402b5494443138
yo16 <xxyo1xx@gmail.com>
2023-11-05 12:50:05 +0900

    Parseできていなかった

--- sql_parser_app/src/components/AstProvider.js
+++ sql_parser_app/src/components/AstProvider.js
@@ -2,6 +2,7 @@ import React, { createContext, useState, useContext } from "react";
 import {v4} from "uuid";
 
 import ParsedSql from "./ParsedSql.js";
+import CalcBoxPos from "./calcBox/CalcBoxPos.js";
 
 const AstContext = createContext();
 export const useAst = () => useContext(AstContext);
@@ -11,13 +12,16 @@ export default function AstProvider({ children }) {
     const [ asts, setAsts ] = useState([]);
 
     const addQuery = (fileName, query) => {
+        const ast = ParsedSql({query});
         setAsts(
             [
                 ...asts,
                 {
                     id: v4(),
                     fileName,
-                    ast:ParsedSql(query)
+                    ast: ast,
+                    query: query,
+                    box: CalcBoxPos(ast)
                 }
             ]
         );
---
+++ sql_parser_app/src/components/calcBox/CalcBoxPos.js
@@ -0,0 +1,26 @@
+// QueryのAstを読んで位置を返す
+/*
+SQL
+    Table
+        Title
+        Columns
+            Column
+
+box : {
+    top,
+    bottom,
+    left,
+    right,
+    width,
+    height,
+}
+*/
+
+// astに含まれているすべてのテーブルの高さを取得
+function getTablesHeight(ast){
+    
+}
+
+export default function CalBoxPos({ ast }) {
+    return {a:1};
+}
